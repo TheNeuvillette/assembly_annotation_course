4c7b190b91e56ea4c5b58fe4685ee45007b5a2da
TheNeuvillette <161205278+TheNeuvillette@users.noreply.github.com>
2025-09-22 19:34:46 +0200

    Add files via upload

---
+++ 01_FastQC_before_FastP/01_FastQC_WGS.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=FastQC
+#SBATCH --time=01:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=1G
+#SBATCH --partition=pibu_el8
+
+# define the path to the diffrent directories, including the container directory
+WORKDIR=/data/users/jimhof/assembly_annotation_course
+WGSDIR=$WORKDIR/00_Sequencing_Data/Etna-2/ERR11437333.fastq.gz
+OUTDIR=$WORKDIR/01_FastQC_MultiQC
+CONTAINERDIR=/containers/apptainer/fastqc-0.12.1.sif
+
+# run fastqc in the container using appainter
+# the bind option gives access to the working directory 
+apptainer exec --bind $WORKDIR $CONTAINERDIR \
+ fastqc -t 2 -o $OUTDIR $WGSDIR
